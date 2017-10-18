427f30f88383c586bc4074fe3d95da7526d5ef7a
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-10-18 23:02:44 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -55,8 +55,8 @@ def adding_matz
           :languages => ["C"]
         }
      }
-  programmer_hash[:yukihiro_matsumoto] << {{:known_for => "Ruby"}, {:languages => ["LISP", "C"]}}
-  puts programmer_hash
+  programmer_hash[:yukihiro_matsumoto] = {{:known_for => "Ruby"}, {:languages => ["LISP", "C"]}}
+  programmer_hash
 
 end
 
