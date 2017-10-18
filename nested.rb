d5db0fd0bc555e09c9a1b962e211c877175ffb3a
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-10-18 22:40:19 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -30,7 +30,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-		 programmer_hash[:dennis_ritchie][:languages].string
+		 programmer_hash[:dennis_ritchie][:languages].first
 end
 
 def adding_matz
