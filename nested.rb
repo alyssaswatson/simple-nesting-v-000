d196ef943877f59f49ea6edede86ac1c7186320e
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-10-18 22:34:47 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -5,8 +5,8 @@ def hopper
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
         }
-				programmer_hash[:grace_hopper]
 			}
+				programmer_hash[:grace_hopper]
     end
 
 def alan_kay_is_known_for
