35755b289c3de663bccc00474aa89a7ce9912a5f
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-10-18 22:30:53 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -4,17 +4,9 @@ def hopper
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
         }
-     }
-end
+				
+    end
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
